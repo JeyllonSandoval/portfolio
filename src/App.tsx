335d0f7708a10633b1691f8ab5100d68552f6eed
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-11-15 21:23:32 -0400

    Integrate @radix-ui/react-dropdown-menu dependency and update package-lock.json. Refactor App component to utilize ThemeProvider and Home page, adding ModeToggle to enhance UI functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme/theme-provider"
+import { Home } from "./pages/home"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <Home />
+    </ThemeProvider>
   )
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/ui/mode-toggle"
+
 export const Home = () => {
     return (
         <div>
             <h1>HOME</h1>
+            <ModeToggle />
         </div>
     )
 }
